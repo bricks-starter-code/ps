1e83c44ea1bbacfc3fe6b54a830df52b05d7791e
B Ricks <bricks@unomaha.edu>
2018-07-28 14:06:20 -0500

    Adding a mouse-based camera

--- canvas.js
+++ canvas.js
@@ -2,8 +2,21 @@ document.body.innerHTML += "<canvas id='canv'></canvas>";
 document.body.style.setProperty('margin', '0px');
 document.documentElement.style.setProperty('margin', '0px'); //Apparently you can't do document.html...https://stackoverflow.com/questions/9362907/how-can-i-reference-the-html-elements-corresponding-dom-object
 
+document.getElementById('canv').addEventListener('mousemove', mouseMove);
+document.getElementById('canv').addEventListener('mousedown', mouseDown);
+document.getElementById('canv').addEventListener('mouseup', mouseUp);
+
+
 var width = 0;  //Will store the width of the canvas
 var height = 0; //Will store the hegiht of the canvas
+
+var cameraCenterX = 0; //The x position the camera is looking at
+var cameraCenterY = 0; //The y position the camera is looking at
+
+var isMouseDown = false; //True if the mouse is down
+
+var lastMouseX = 0;
+var lastMouseY = 0;
 
 ///This gets called once when the page is completetly loaded.
 ///Think main()
@@ -62,11 +75,54 @@ function drawCanvas() {
   ///Clear the rectangles
   ctx.fillStyle = "gray";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  ctx.save();
+  
+  ctx.translate(width/2 - cameraX, height/2 - cameraY);
+  
+  
 
   if(typeof customDraw === "function"){
     customDraw(ctx);
   }
+  
+  ctx.restore();
 
 }
+
+function mouseMove(e){
+  let currentMouseX = e.clientX;
+  let currentMouseY = e.clientY;
+  
+  lastMouseX = e.clientX;
+  lastMouseY = e.clientY;
+}
+
+function mouseDown(e){
+  let currentMouseX = e.clientX;
+  let currentMouseY = e.clientY;
+  
+  if(isMouseDown){
+    let diffX = currentMouseX - lastMouseX;
+    let diffY = currentMouseY = lastMouseY;
+    
+    cameraX += diffX;
+    cameraY += diffY;
+  }
+  
+  lastMouseX = e.clientX;
+  lastMouseY = e.clientY;
+  isMouseDown = true;
+}
+
+function mouseUp(e){
+   let currentMouseX = e.clientX;
+  let currentMouseY = e.clientY;
+  
+  lastMouseX = e.clientX;
+  lastMouseY = e.clientY;
+  isMouseDown = false
+}
+
 
 initialBoot();

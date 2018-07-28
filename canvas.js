1c80e5330bb5b435ab12a2c40434173aea58a7f6
B Ricks <bricks@unomaha.edu>
2018-07-28 14:31:31 -0400

    Getting rid of margins (#1)
    * Getting rid of margins
    
    * Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,4 +1,6 @@
 document.body.innerHTML += "<canvas id='canv'></canvas>";
+document.body.style.setProperty('margin', '0px');
+document.documentElement.style.setProperty('margin', '0px'); //Apparently you can't do document.html...https://stackoverflow.com/questions/9362907/how-can-i-reference-the-html-elements-corresponding-dom-object
 
 ///This gets called once when the page is completetly loaded.
 ///Think main()
@@ -51,7 +53,7 @@ function drawCanvas() {
   var ctx = canvas.getContext("2d");      ///Get the canvas context
 
   ///Clear the rectangles
-  ctx.fillStyle = "yellow";
+  ctx.fillStyle = "gray";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   if(typeof customDraw === "function"){

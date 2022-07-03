7c30e9e69fc490b6fceac6a0506178c15f3aa422
Ricks <bricks@unomaha.edu>
2022-07-03 15:40:04 -0500

    Disable Right Mouse Popup

--- canvas.js
+++ canvas.js
@@ -14,7 +14,7 @@
 
 
 
- document.body.innerHTML += "<canvas id='canv'></canvas>";
+ document.body.innerHTML += "<canvas id='canv' oncontextmenu='return false;'></canvas>";
  document.body.style.setProperty('margin', '0px');
  document.documentElement.style.setProperty('margin', '0px'); //Apparently you can't do document.html...https://stackoverflow.com/questions/9362907/how-can-i-reference-the-html-elements-corresponding-dom-object
  document.documentElement.style.setProperty('overflow', 'hidden'); //Apparently you can't do document.html...https://stackoverflow.com/questions/9362907/how-can-i-reference-the-html-elements-corresponding-dom-object

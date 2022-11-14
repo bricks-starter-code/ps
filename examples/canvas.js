1844e70f06d72969bde6a3144d7f50373294d020
bricksphd <bricks@unomaha.edu>
2022-11-13 19:41:01 -0600

    Working on intelligent zoom;

--- examples/canvas.js
+++ examples/canvas.js
@@ -57,7 +57,9 @@ o.lastMouseX = 0;
 o.lastMouseY = 0;
 
 //Set the default camera zoom
-o.cameraZoom = 1;
+o.maxZoom = 100;
+o.minZoom = .1
+o.cameraZoom = o.maxZoom;
 
 //Set the default background color
 o.fillColor = "lightgray"
@@ -65,6 +67,7 @@ o.fillColor = "lightgray"
 //Set the frame rate
 o.millisecondsBetweenFrames = 33
 o.secondsBetweenFrames = 1 / o.millisecondsBetweenFrames
+o.time = o.secondsBetweenFrames;
 
 ///Uncomment to disable mouse panning and zooming
 //o.ignoreEvents = true;
@@ -74,6 +77,10 @@ o.secondsBetweenFrames = 1 / o.millisecondsBetweenFrames
 
 let c;
 let canvas
+
+canvas = document.getElementById("canv");   ///Get the canvas object
+c = canvas.getContext("2d");
+
 
 o.toWorldSpace = function (screenX, screenY) {
   let x = screenX - (o.width / 2 - o.cameraCenterX);
@@ -91,50 +98,74 @@ o.toScreenSpace = function (worldX, worldY) {
   return [x, y];
 }
 
+o.lookAt = function(x, y){
+  o.cameraCenterX = x * o.cameraZoom;
+  o.cameraCenterY = y * -o.cameraZoom;
+  return this;
+}
+
+o.zoom = function(z){
+  let h = c.canvas.height;
+  z = h/(2)
+  // if(z > o.maxZoom)
+  //   z = o.maxZoom;
+  // if(z < o.minZoom)
+  //   z = o.minZoom;
+  
+  o.cameraZoom = z;
+  return this;
+}
+
+c.b = function () {
+  c.beginPath();
+  return this;
+}
+c.m = function (x, y) {
+  c.moveTo(x, y)
+  return this;
+}
+c.l = function (x, y) {
+  c.lineTo(x, y);
+  return this;
+}
+c.s = function () {
+  c.stroke();
+  return this;
+}
+c.f = function(){
+  c.fill();
+  return this;
+}
+c.st = function (style) {
+  c.strokeStyle = style;
+  return this;
+}
+c.fi = function (style) {
+  c.fillStyle = style;
+  return this;
+}
+c.fo = function (font) {
+  c.font = font
+  return this;
+}
+c.text = function (text, x, y) {
+  c.fillText(text, x, y);
+  return this;
+}
+c.circle = function(x,y,r){
+  c.arc(x,y,r, 0, Math.PI * 2)
+  return this;
+}
+
 ///This gets called once when the page is completetly loaded.
 ///Think main()
 function initialBoot() {
 
-  canvas = document.getElementById("canv");   ///Get the canvas object
-  c = canvas.getContext("2d");
-  c.b = function () {
-    c.beginPath();
-    return this;
-  }
-  c.m = function (x, y) {
-    c.moveTo(x, y)
-    return this;
-  }
-  c.l = function (x, y) {
-    c.lineTo(x, y);
-    return this;
-  }
-  c.s = function () {
-    c.stroke();
-    return this;
-  }
-  c.st = function (style) {
-    c.strokeStyle = style;
-    return this;
-  }
-  c.fi = function (style) {
-    c.fillStyle = style;
-    return this;
-  }
-  c.fo = function (font) {
-    c.font = font
-    return this;
-  }
-  c.text = function (text, x, y) {
-    c.fillText(text, x, y);
-    return this;
-  }
-
-
+  i.attach(document)
 
   //Call the firstUpdate function if it exists (only called once)
   if (typeof firstUpdate === "function")
-    firstUpdate(o);
+    firstUpdate(c, o);
 
 
   ///Start a timer
@@ -178,9 +209,11 @@ function update() {
   canvas.width = o.width;
   canvas.height = o.height;
 
+  i.update();
+
   //If there is a custom update function, call it.
   if (typeof customUpdate === "function") {
-    customUpdate(o);
+    customUpdate(c, o);
   }
 
   drawCanvas();       ///Draw the canvas
@@ -195,6 +228,8 @@ function drawCanvas() {
   //Draw a grid in UI space before anythig else if the user
   //has requested it
   if (o.drawGrid) {
+    c.fo("10px Arial").fi("white")
+      
 
     //The coordinates of the upper left (ul) and lower right (lr) coordinates
     let ulx, uly;
@@ -208,7 +243,7 @@ function drawCanvas() {
     let base = 10;
     let min = Math.min(lrx - ulx, lry - uly);
     let step = Math.log10(min) / Math.log10(base);
-    step = Math.floor(step);
+    step = Math.floor(step)-1;
     step = Math.pow(base, step);
 
     startX = parseInt((ulx - step) / step) * step
@@ -229,7 +264,7 @@ function drawCanvas() {
       c.stroke()
 
       c.fillStyle = "white"
-      c.fillText(x, tx + 20, 20);
+      c.fillText(x.toFixed(2), tx + 20, 20);
     }
 
     for (let y = startY; y <= stopY; y += step) {
@@ -245,7 +280,7 @@ function drawCanvas() {
       c.stroke()
 
       c.fillStyle = "white"
-      c.fillText((-y), 20, ty + 20);
+      c.fillText((-y).toFixed(2), 20, ty + 20);
     }
   }
 
@@ -319,17 +354,17 @@ function mouseWheel(e) {
   //y -= (height/2 - o.cameraCenterY);
 
   if (e.wheelDelta > 0) {
-    o.cameraZoom *= 1.1;
+    o.cameraZoom *= 1.01;
   }
   else if (e.wheelDelta < 0) {
-    o.cameraZoom /= 1.1;
+    o.cameraZoom /= 1.01;
   }
 
-  if (o.cameraZoom > 100) {
-    o.cameraZoom = 100;
+  if (o.cameraZoom > o.maxZoom) {
+    o.cameraZoom = o.maxZoom;
   }
-  if (o.cameraZoom < .1) {
-    o.cameraZoom = .1
+  if (o.cameraZoom < o.minZoom) {
+    o.cameraZoom = o.minZoom
   }
 
   //Now figure out what the new world space coordinate has changed to
@@ -349,5 +384,154 @@ function noEvents() {
   return (typeof o.ignoreEvents !== "undefined" && o.ignoreEvents)
 }
 
+class i {
+  static keys = []; // Keys that are currently down as reported by js events
+  static keysDown = []; //Keys that went down this frame as reported by js events
+  static keysUp = []; //Keys that went up this frame as reported by js events
+  static frameKeysDown = []; //Keys that will be reported as going down next frame
+  static frameKeysUp = [];  //Keys that will be reported as going up next frame
+
+  static mouseButtons = []; //Mouse buttons that are currently down as reported by js events
+  static mouseButtonsDown = []; //Mouse buttons that went down sometime this frame as reported by js events
+  static mouseButtonsUp = []; //Mouse buttons that went up sometime this frame as reported by js events
+  static frameMouseButtonsDown = []; //Mouse buttons that will be reported as going down next frame
+  static frameMouseButtonUp = []; //Mouse buttons that will be reported as going up next frame
+
+  static mousePositionX; //Mouse position x as reported by js events
+  static mousePositionY; //Mouse position y as report by js events
+  static frameMousePositionX; //The mouse position x that will be reported next frame
+  static frameMousePositionY; //The mouse position y that will be reported next frame
+  static lastFrameMousePositionX; //The mouse x position from the previously reported frame
+  static lastFrameMousePositionY; //The mouse y position from the previously reported frame
+
+  static scrollWheel = 0; //The scroll wheel position as report by js events
+  static frameScrollWheel = 0; //The scroll wheel change that will be reported next frame
+
+  //Update the frame-centric variables
+  static update() {
+    this.frameKeysDown = this.keysDown;
+    this.frameKeysUp = this.keysUp;
+    this.keysDown = [];
+    this.keysUp = [];
+
+    this.frameMouseButtonsDown = this.mouseButtonsDown;
+    this.frameMouseButtonsUp = this.mouseButtonsUp;
+    this.mouseButtonsDown = [];
+    this.mouseButtonsUp = [];
+
+    this.lastFrameMousePositionX = this.frameMousePositionX
+    this.lastFrameMousePositionY = this.frameMousePositionY
+
+    this.frameMousePositionX = this.mousePositionX;
+    this.frameMousePositionY = this.mousePositionY;
+
+    this.frameScrollWheel = this.scrollWheel;
+    this.scrollWheel = 0;
+  }
+
+  //Get the values of different input states
+
+  static getKey(key) {
+    return this.keys[key];
+  }
+  static getKeyDown(key) {
+    return this.frameKeysDown[key];
+  }
+  static down(key, lambda){
+    if(i.getKeyDown(key))
+      lambda();
+  }
+  static getKeyUp(key) {
+    return this.frameKeysUp[key];
+  }
+
+  static getMouseButton(button) {
+    return this.mouseButtons[button];
+  }
+  static getMouseButtonDown(button) {
+    return this.frameMouseButtonsDown[button];
+  }
+  static getMouseButtonUp(button) {
+    return this.frameMouseButtonsUp[button];
+  }
+
+  static getScrollWheel() {
+    return this.frameScrollWheel;
+  }
+
+  static getMousePosition() {
+    return { x: this.frameMousePositionX, y: this.frameMousePositionY };
+  }
+  static getMousePositionDelta() {
+    return { dx: this.frameMousePositionX - this.lastFrameMousePositionX, dy: this.frameMousePositionY - this.lastFrameMousePositionY };
+  }
+
+
+  static attach(document) {
+    //Setup all the key listeners
+    document.body.addEventListener('keydown', keydown);
+    document.body.addEventListener('keyup', keyup);
+    document.body.addEventListener('keypress', keypress);
+    document.body.addEventListener('mousedown', mousedown);
+    document.body.addEventListener('mouseup', mouseup);
+    document.body.addEventListener('mousemove', mousemove);
+    document.body.addEventListener('wheel', wheelevent);
+    document.body.addEventListener('contextmenu', contextmenu);
+
+    function keydown(event) {
+      //console.log("Down: " + event.key);
+      if (i.keys[event.key] != true)
+        i.keysDown[event.key] = true;
+      i.keys[event.key] = true;
+    }
+
+    function keyup(event) {
+      //console.log("Up: " + event.key)
+      if (i.keys[event.key] != false)
+        i.keysUp[event.key] = true;
+      i.keys[event.key] = false;
+    }
+
+    function mousedown(event) {
+      //console.log("Mouse Down: " + event.button)
+      if (i.mouseButtons["" + event.button] != true)
+        i.mouseButtonsDown["" + event.button] = true;
+      i.mouseButtons["" + event.button] = true;
+    }
+
+    function mouseup(event) {
+      //console.log("Mouse Up: " + event.button)
+      if (i.mouseButtons[event.button] != false)
+        i.mouseButtonsUp[event.button] = true;
+      i.mouseButtons[event.button] = false;
+    }
+
+    function mousemove(event) {
+      //console.log("Mouse Move: " + event.clientX + ", " + event.clientY)
+      i.mousePositionX = event.clientX;
+      i.mousePositionY = event.clientY;
+    }
+
+    function wheelevent(event) {
+      //console.log("Scroll Delta: " + event.deltaY)
+      if (event.deltaY != 0)
+        i.mouseScrollDelta = event.deltaY;
+    }
+
+    function keypress(event) {
+      //console.log(`Keys: ${event.key}, Modifier keys: Control: ${event.ctrlKey}, Alt: ${event.altKey}, Shift: ${event.shiftKey}, Meta Key: ${event.metaKey}`);
+    }
+
+    // Based on https://stackoverflow.com/questions/381795/how-to-disable-right-click-context-menu-in-javascript
+    // Kills the right mouse context menu
+    function contextmenu(event) {
+      if (event.preventDefault != undefined)
+        event.preventDefault();
+      if (event.stopPropagation != undefined)
+        event.stopPropagation();
+      return false;
+    }
+  }
+}
 
 initialBoot();

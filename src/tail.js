f9188e3f4663bed4176a47872c8e7d3555a0b79f
bricksphd <bricks@unomaha.edu>
2022-11-27 14:32:04 -0600

    Add some code simplification

--- src/tail.js
+++ src/tail.js
@@ -3,4 +3,20 @@ function collisionRectRect(cx, cy, rx, ry, cx1, cy1, rx1, ry1) {
   return collision;
 }
 
+function inc(current, change, max, ifTrue, ifFalse){
+  let toReturn = current + change;
+  if(toReturn >= max){
+    if(ifTrue){
+      ifTrue()
+    }
+    return 0;
+  }
+  else{
+    if(ifFalse){
+      ifFalse()
+    }
+    return toReturn
+  }
+}
+
 initialBoot();
